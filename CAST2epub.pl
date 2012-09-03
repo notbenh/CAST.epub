5664aee54840678d3d445df8924b07a28c37945a
ben hengst <notbenh@cpan.org>
2012-09-02 22:57:10 -0700

    giving this epub thing another shot

--- CAST2epub.pl
+++ CAST2epub.pl
@@ -3,54 +3,58 @@ use strict;
 use warnings;
 use EBook::EPUB;
 use Getopt::Long;
-my $race = 'race';
-GetOptions ( "race=s" => \$race );
+my $race   = 'race';
+my $height = 600;
+my $width  = 800;
+my $fg     = 'white';
+my $bg     = 'black';
+my $font_file = 'Impact.ttf';
+my $font_factor = 1.1;
+
+GetOptions ( "race=s"   => \$race 
+           , "height=i" => \$height
+           , "width=i"  => \$width
+           , "fg=s"     => \$fg
+           , "bg=s"     => \$bg
+           , "font=s"   => \$font_file
+           , "factor=s" => \$font_factor
+           );
+use Imager;
+ 
+my $font = Imager::Font->new(file=> $font_file)
+       or die "Cannot load $font_file ", Imager->errstr;
+ 
+mkdir($race);
 
 my $epub = EBook::EPUB->new;
- 
-# Set metadata: title/author/language/id
-$epub->add_title(sprintf "CAST: $race [%s]", localtime);
 $epub->add_language('en');
- 
-my $page = 1;
-foreach my $data (@ARGV) {
-  warn "DATA: $data\n";
-  $epub->add_navpoint( label       => "CAST $page"
-                     , id          => $epub->copy_file("./$data", $data, 'image/jpeg') # TODO
-                     , content     => $data
-                     , play_order  => $page 
-                     );
-  $page++;
+$epub->add_title(sprintf "CAST: $race [%s]", localtime);
+
+my $i = 1;
+foreach my $CAST (@ARGV) {
+  my $image = Imager->new(xsize => $width, ysize => $height);
+  $image->box(filled=>1, color=>$bg); #background
+   
+  $font->align( string => $CAST,
+              , size => $height/$font_factor
+              , color => $fg
+              , x => $image->getwidth/2
+              , y => $image->getheight/2
+              , halign => 'center'
+              , valign => 'center'
+              , image => $image
+              );
+   
+  my $filename = sprintf "%s/%04d.jpg", $race, $i;
+  $image->write(file=>$filename)
+      or die "Cannot save $filename ", $image->errstr;
+  my $id = $epub->copy_image($filename, "$i.jpg", "image/jpeg");
+  #my $data;
+  #$image->write(data=>\$data, type=>"jpeg")
+  #    or die "Cannot save $filename ", $image->errstr;
+  #my $id = $epub->add_image($filename, $data, "image/jpeg");
+  
+  $i++;
 }
   
 $epub->pack_zip("CAST_$race.epub");
-__END__
-
-#$epub->add_author('Jerome K. Jerome');
-#$epub->add_identifier('1440465908', 'ISBN');
-# Add package content: stylesheet, font, xhtml and cover
-#$epub->copy_stylesheet('/path/to/style.css', 'style.css');
-
-
-
-#$epub->copy_file('/path/to/figure1.png', 'figure1.png', 'image/png');
-#$epub->encrypt_file('/path/to/CharisSILB.ttf', 'CharisSILB.ttf', 'application/x-font-ttf');
-#my $chapter_id = $epub->copy_xhtml('/path/to/page1.xhtml', 'page1.xhtml');
-#$epub->copy_xhtml('/path/to/notes.xhtml', 'notes.xhtml', linear => 'no');
- 
-# Add top-level nav-point
-my $navpoint = $epub->add_navpoint(
-        label       => "Chapter 1",
-        id          => $chapter_id,
-        content     => 'page1.xhtml',
-        play_order  => 1 # should always start with 1
-);
- 
-# Add cover image
-# Not actual epub standart but does the trick for iBooks
-#my $cover_id = $epub->copy_image('/path/to/cover.jpg', 'cover.jpg');
-#$epub->add_meta_item('cover', $cover_id);
- 
-
-
-
